997bcaa0c1104beda2c8dfb3129de88f3eccd67b
tamas-rabel <tamas.rabel@gmail.com>
2023-01-31 23:48:57 +0000

    Switching test to card-badges

--- trello/client.js
+++ trello/client.js
@@ -1,8 +1,7 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
+  'card-badges': function(t, options){
     return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Estimate Size',
+      text: '1/2 Estimate **Size**',
     }];
   },
 });
