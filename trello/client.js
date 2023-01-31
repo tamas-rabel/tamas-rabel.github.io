76f65e1d806d7197302c7bb2f981c20b305dbef1
tamas-rabel <tamas.rabel@gmail.com>
2023-01-31 23:39:31 +0000

    Trello power-up test

---
+++ trello/client.js
@@ -0,0 +1,8 @@
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options){
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Estimate Size',
+    }];
+  },
+});
