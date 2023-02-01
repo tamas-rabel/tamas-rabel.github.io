40a3e18dd70c01e5668c44c34dde9dccf0c22cd9
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 00:21:57 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -1,7 +1,21 @@
 TrelloPowerUp.initialize({
   'card-badges': function(t, options){
-    return [{
-      text: '1/2 Estimate **Size** <p>test</p>',
+      console.log(options);
+      console.log(t.get('card', 'shared', 'checklist'));
+    return [
+    {
+      color: 'light-gray',
+      text: '1/2 Initial spike',
+    },
+    {
+      text: '☐ Peer to peer vs client-server?',
+    },
+    {
+      text: '☑ Check this item',
     }];
   },
 });
+
+
+// ☐ Peer to peer vs client-server?                                                                                                    
+// ☑ Check this item                                                                                                    
