9ed5016281a43ee09714b10cd3b5edee3a734ee9
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 00:43:49 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -2,13 +2,12 @@ TrelloPowerUp.initialize({
   'card-badges': function(t, options){
       console.log(options);
     t.card('id', 'checklists').then(function (card) {
-        console.log(card.checklists)
         if (card.checklists.length > 0)
         {
             console.log(card.checklists[0].id)
-//            fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
-//            .then((response) => response.json())
-//            .then((json) => console.log(json));
+            fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
+            .then((response) => response.json())
+            .then((json) => console.log(json));
         }
     });
     
