b37b08740ed7e5f47b5cd0702897db1b026f27a0
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 19:57:45 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -125,7 +125,7 @@ TrelloPowerUp.initialize(
             {
                 type: 'iframe',
                 icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
-                url: t.signUrl('subtasks-section.html'),
+                url: './subtasks-section.html',
                 height: 256,
             }
         }];
