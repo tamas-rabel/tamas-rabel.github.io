51ba4a716f04ac029f48c4b4d02ae237b9f3f2e1
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 01:39:56 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -1,55 +1,42 @@
-console.log(window.TrelloPowerUp);
-var t = window.TrelloPowerUp.iframe({
-    appKey: '2673af39e812244706daa1292a259359',
-    appName: 'mb',
-});
-console.log("Checking authorization")
-t.getRestApi().isAuthorized().then(function(isAuthorized)
+TrelloPowerUp.initialize(
 {
-    console.log("Answer received")
-    if (!isAuthorized)
+    'card-badges': function(t, options)
     {
-        console.log("Not authorized")
-        t.getRestApi().authorize({ scope: 'read,write' }).then(function(t)
+        t.card('id', 'checklists').then(function (card)
         {
-            alert('Success!');
-        });
-    }
-    else
-    {
-        console.log("Authorized")
-        TrelloPowerUp.initialize(
-        {
-            'card-badges': function(t, options)
+            if (card.checklists.length > 0)
             {
-                t.card('id', 'checklists').then(function (card)
-                {
-                    if (card.checklists.length > 0)
-                    {
-                        console.log(card.checklists[0])
-                        console.log(card.checklists[0].id)
-                        fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
-                        .then((response) => response.json())
-                        .then((json) => console.log(json));
-                    }
-                });
+                console.log(card.checklists[0])
+                console.log(card.checklists[0].id)
+                fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+            }
+        });
 
-                return [
-                {
-                    color: 'light-gray',
-                    text: '1/2 Initial spike',
-                },
-                {
-                    text: '☐ Peer to peer vs client-server?',
-                },
-                {
-                    text: '☑ Check this item',
-                }];
-            },
+        return [
+        {
+            color: 'light-gray',
+            text: '1/2 Initial spike',
         },
         {
-            appKey: '2673af39e812244706daa1292a259359',
-            appName: 'mb',
-        });
-    }
+            text: '☐ Peer to peer vs client-server?',
+        },
+        {
+            text: '☑ Check this item',
+        }];
+    },
+},
+{
+    'board-buttons': function (t, opts)
+    {
+        return [
+        {
+            text: 'mb',
+            url: 'https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=2673af39e812244706daa1292a259359',
+        }];
+    },
+{
+    appKey: '2673af39e812244706daa1292a259359',
+    appName: 'mb'
 });
