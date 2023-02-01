5087383f086ac6f7bc5fd37165a631c211e902ca
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 01:14:39 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -1,30 +1,36 @@
-TrelloPowerUp.initialize({
-  'card-badges': function(t, options){
-    t.card('id', 'checklists').then(function (card) {
-        if (card.checklists.length > 0)
+TrelloPowerUp.initialize(
+{
+    'card-badges': function(t, options)
+    {
+        return t.getRestApi().isAuthorized().then(function(isAuthorized)
         {
-            console.log(card.checklists[0])
-            console.log(card.checklists[0].id)
-            fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
-            .then((response) => response.json())
-            .then((json) => console.log(json));
-        }
-    });
-    
-    return [
-    {
-      color: 'light-gray',
-      text: '1/2 Initial spike',
+            t.card('id', 'checklists').then(function (card)
+            {
+                if (card.checklists.length > 0)
+                {
+                    console.log(card.checklists[0])
+                    console.log(card.checklists[0].id)
+                    fetch('https://api.trello.com/1/checklists/' + card.checklists[0].id + '/checkItems')
+                    .then((response) => response.json())
+                    .then((json) => console.log(json));
+                }
+            });
+        });
+
+        return [
+        {
+            color: 'light-gray',
+            text: '1/2 Initial spike',
+        },
+        {
+            text: '☐ Peer to peer vs client-server?',
+        },
+        {
+            text: '☑ Check this item',
+        }];
     },
-    {
-      text: '☐ Peer to peer vs client-server?',
-    },
-    {
-      text: '☑ Check this item',
-    }];
-  },
+},
+{
+    appKey: '2673af39e812244706daa1292a259359',
+    appName: 'mb'
 });
-
-
-// ☐ Peer to peer vs client-server?                                                                                                    
-// ☑ Check this item                                                                                                    
