b9f02a2e468da704cf92e1b659e6ec691f2b8f2c
tamas-rabel <tamas.rabel@gmail.com>
2023-02-01 16:19:42 +0000

    Add files via upload

--- trello/client.js
+++ trello/client.js
@@ -69,8 +69,8 @@ TrelloPowerUp.initialize(
 
             await t.cards('id', 'name', 'cover').then(function(cards)
             {
-                let subtasks = t.get('card', 'shared', 'mb-subtasks', new Array());
-                console.log(subtasks);
+                let subtasks = t.get('card', 'shared', 'mb-subtasks', []);
+                console.log(typeof(subtasks));
                 for (let s=0; s<subtasks.length; s++)
                 {
                     card = cards.find((c) => c.id == subtasks[s])
